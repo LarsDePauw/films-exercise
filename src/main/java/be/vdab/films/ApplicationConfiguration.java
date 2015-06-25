795d97c0cfbccf474b797365a0820b4c0f2bc6dc
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-06-25 12:36:27 +0200

    Enabled controllers

--- src/main/java/be/vdab/films/ApplicationConfiguration.java
+++ src/main/java/be/vdab/films/ApplicationConfiguration.java
@@ -1,7 +1,9 @@
 package be.vdab.films;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("be.vdab")
 public class ApplicationConfiguration {
 }
