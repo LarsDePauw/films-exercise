3a629265a5a76c3893841cf17c3d8561edda13ca
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-06-25 13:39:08 +0200

    Added persistence layer using JPA

--- src/main/java/be/vdab/films/FilmController.java
+++ src/main/java/be/vdab/films/FilmController.java
@@ -1,11 +1,15 @@
 package be.vdab.films;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FilmController {
+    @Autowired
+    FilmRepository filmRepository;
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
